a09d9b27b64559ec940fb1c6d4ebe2548d933c39
Aleksei Krugliak <aleksei.krugliak@altenar.com>
2022-03-26 17:45:56 +0400

    fixes

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# how to use
+
+[//]: # (python3 -m venv env)
+
+[//]: # (source env/bin/activate)
+
+[//]: # (pip install --editable .)
+
+[//]: # (deactivate)
--- pythoneer.py
+++ pythoneer.py
@@ -2,43 +2,44 @@
 import cmd
 import os
 
+
 def draw_map():
-	print("this is map tmp...")
+    print("this is map tmp...")
 
 
 class Cli(cmd.Cmd):
 
-	def __init__(self):
-		cmd.Cmd.__init__(self)
-		self.prompt = ">>> "
-		self.intro = "Добро пожаловать\nДля справки наберите 'help'"
-		self.doc_header = "Доступные команды (для справки по конкретной команде наберите 'help _команда_')"
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = ">>> "
+        self.intro = "Добро пожаловать\nДля справки наберите 'help'"
+        self.doc_header = "Доступные команды (для справки по конкретной команде наберите 'help _команда_')"
 
-	def do_show_disk(self, args):
-		"""show_disk - свободное место на диске"""
-		os.system("df -h")
+    def do_show_disk(self, args):
+        """show_disk - свободное место на диске"""
+        os.system("df -h")
 
-	def do_map(self, line):
-		"""show - showing map."""
-		os.system("clear")
-		draw_map()
+    def do_map(self, line):
+        """show - showing map."""
+        os.system("clear")
+        draw_map()
 
-	def do_exit(self, line):
-		"""exit - exit program."""
-		print("Bye!")
-		return True
+    def do_exit(self, line):
+        """exit - exit program."""
+        print("Bye!")
+        return True
 
-	def default(self, line):
-		os.system("clear")
-		print(f'[{line}] Wrong command.')
+    def default(self, line):
+        os.system("clear")
+        print(f'[{line}] Wrong command.')
 
-	def emptyline(self):
-		pass
+    def emptyline(self):
+        self.default("empty")
 
 
 if __name__ == "__main__":
-	cli = Cli()
-	try:
-		cli.cmdloop()
-	except KeyboardInterrupt:
-		print("завершение сеанса...")
+    cli = Cli()
+    try:
+        cli.cmdloop()
+    except KeyboardInterrupt:
+        print("завершение сеанса...")
